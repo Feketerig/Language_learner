e865bec9cb849442f0084b99c26a4ec456ba67eb
Fazekas Levente <levente.fazekas@gmail.com>
2022-11-15 16:39:42 +0100

    Added saving word

--- app/src/main/java/hu/bme/aut/android/languagelearner/data/network/WordApiImpl.kt
+++ app/src/main/java/hu/bme/aut/android/languagelearner/data/network/WordApiImpl.kt
@@ -8,36 +8,6 @@ import io.ktor.client.request.*
 class WordApiImpl(
     private val client: HttpClient
 ): WordApi {
-    /*override suspend fun getAllWordSets(): List<WordSetDTO> {
-        return listOf(
-            WordSetDTO(
-                id = 1,
-                title = "Title",
-                description = "Description - optional",
-                words = listOf(WordPairDTO(1,"magyar", "hungarian", false)),
-                tags = listOf(WordSetTagDTO(10,"angol"), WordSetTagDTO(9,"magyar"))
-            ),
-            WordSetDTO(
-                id = 2,
-                title = "Title 2",
-                description = "Description - optional",
-                words = listOf(WordPairDTO(2,"angol", "english", true), WordPairDTO(1,"magyar", "hungarian", false)),
-                tags = listOf(WordSetTagDTO(5,"német"), WordSetTagDTO(8,"magyar"))
-            ),
-            WordSetDTO(
-                id = 3,
-                title = "Title 3",
-                description = "Description - optional",
-                words = listOf(WordPairDTO(1,"magyar", "hungarian", false)),
-                tags = listOf(WordSetTagDTO(1,"angol"), WordSetTagDTO(2,"magyar"))
-            )
-        )
-        return emptyList()
-    }*/
-
-    override suspend fun updateWordMemorized(wordPairId: Int, memorized: Boolean) {
-
-    }
 
     override suspend fun login(email: String, password: String): LoginResponse {
         return client.post(urlString = WordApi.Endpoints.Auth.url + "/login"){
@@ -69,6 +39,10 @@ class WordApiImpl(
                 score = score
             ))
         }
+    }
+
+    override suspend fun updateWordMemorized(wordPairId: Int, memorized: Boolean) {
+        client.post(WordApi.Endpoints.Words.url + "/save/" + wordPairId)
     }
 
     override suspend fun setName(name: String){
